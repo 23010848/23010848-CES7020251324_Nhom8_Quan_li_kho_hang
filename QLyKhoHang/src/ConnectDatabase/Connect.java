ce39ede6ad5a0068e4c433c3fbe78a3a0048df22
23010848 <152244492+23010848@users.noreply.github.com>
2025-06-13 08:17:21 +0700

    Update Connect.java

--- QLyKhoHang/src/ConnectDatabase/Connect.java
+++ QLyKhoHang/src/ConnectDatabase/Connect.java
@@ -1,28 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ConnectDatabase;
 
-/**
- *
- * @author MRSTHAO
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connect {
-    
-    //Connect database for MySQL 
-    public static Connection getConnection(){
+
+    // Connect database for MySQL (XAMPP)
+    public static Connection getConnection() {
         Connection cnt = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            cnt = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlykhohang", "root", "123456");
-            System.out.println("connect successfully!");
-        } catch (Exception e) {
-            System.out.println("connect failure!");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // URL kết nối đến MySQL qua XAMPP
+            String url = "jdbc:mysql://localhost:3307/qlykhohang?useSSL=false&serverTimezone=UTC";
+
+            // Tài khoản mặc định của XAMPP là 'root', không có mật khẩu
+            String user = "root";
+            String password = "";  // nếu có mật khẩu thì nhập vào đây
+
+            cnt = DriverManager.getConnection(url, user, password);
+            System.out.println("Ket noi thanh cong!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Ket noi that bai!");
             e.printStackTrace();
         }
         return cnt;
